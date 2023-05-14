3021f78accac5c4db8dfdb6a570da9128677839f
tipoigorek <shlleg.kul@gmail.com>
2023-05-14 08:56:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function replaceImages() {
+    let images = document.body.getElementsByTagName("img");
+    for (let i = images.length - 1; i >= 0; i--) {
+        let image = images[i];
+        if (image.alt) {
+            let text = document.createTextNode(image.alt);
+            image.parentNode.replaceChild(text, image);
+        }
+    }
+}
